ef16151bb5c811b928fb61795fed80828900ae38
watkinsta <taw46@case.edu>
2017-10-23 19:59:00 -0400

    Added notes

--- GCcontent.py
+++ GCcontent.py
@@ -1,8 +1,8 @@
 DNA = input("Enter DNA sequence:")
-DNA_cap = DNA.upper()
+DNA_cap = DNA.upper() #all letters uppercase
 seqlen = len(DNA)
 Gcontent = DNA_cap.count("G")
 Ccontent = DNA_cap.count("C")
 GC = Gcontent + Ccontent
 GCperc = GC / seqlen * 100
-print("GC Percent:" + str("%.2f" % round(GCperc,2)) + "%")
+print("GC Percent:" + str("%.2f" % round(GCperc,2)) + "%") #Float limited to 2 decimals
